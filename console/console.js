539dc325cbcdb185368f44f6d40fcf52affe906e
Seun0001 <oluwaseunafolabi941@gmail.com>
2024-05-15 00:59:11 +0100

    Add filter,reduce with comment

--- console/console.js
+++ console/console.js
@@ -1,3 +1,4 @@
+// Define the myArrayList variable
 let myArrayList = [];
 myArrayList = [
   {
@@ -8,9 +9,25 @@ myArrayList = [
   },
 ];
 
+// Push two more objects to the myArrayList array
 myArrayList.push(
   { OperatingSystemUsed: "Linux Mint" },
   { CurrentInternCompany: "Flexisaf" }
 );
 
+// Log the array list as a string with each value on a new line
 console.log(myArrayList.map(Object.values).flat().join("\n"));
+
+// Create a new array with the fullName property removed from each item
+const reducedArray = myArrayList
+  .map((item) => {
+    const { fullName, ...rest } = item;
+    return rest;
+  })
+  // Flatten the array of arrays into a single array
+  .reduce((acc, item) => {
+    return [...acc, ...Object.values(item)];
+  }, []);
+
+// Log the reduced array
+console.log(reducedArray);
